6b4f11f9f42a926e23179aa1ed4583e75331ecf0
Andy Agus <Redagus@gmail.com>
2020-06-24 15:43:45 -0400

    added delete method

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -27,8 +27,8 @@ class ArtistsController < ApplicationController
 	end
 
 	def destroy
-		@artist = Artist.find(params[:id])
-		byebug
+	
+		@artist = Artist.find(params[:id])	
 		@artist.destroy
 		redirect_to artists_path
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	resources :artists, only:[:update, :edit, :new, :create, :index, :show] 
 
-	delete  '/artists/:id', to: 'artist#delete', as: :delete_artist
+	delete '/artists/:id', to: 'artists#destroy', as: :delete_artist
 
 
 end
